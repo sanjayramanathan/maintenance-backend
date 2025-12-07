fa55fd297c4344f42a5fefae2d0d2fc05ab7e511
sanjayramanathan <sanjayramanathan2006@gmail.com>
2025-12-07 09:48:02 +0530

    Initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +65,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database - SQLite (Render supports it)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Static files
